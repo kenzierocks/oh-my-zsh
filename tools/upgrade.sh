6ef58cdf9fbb8a9f2a8f12947d5097cd52a09967
Kenzie Togami <ket1999@gmail.com>
2014-08-25 18:54:08 -0700

    Git, not pit

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -22,7 +22,7 @@ fi
 
 printf "${BLUE}%s${NORMAL}\n" "Updating Oh My Zsh"
 cd "$ZSH"
-if git pull --ff-only fetch-origin master && pit push
+if git pull --ff-only fetch-origin master && git push
 then
   printf '%s' "$GREEN"
   printf '%s\n' '         __                                     __   '
