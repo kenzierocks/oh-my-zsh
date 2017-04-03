a0d9fc4ebbebee7d086ffc622d1918f2e7c0000a
Kenzie Togami <ket1999@gmail.com>
2017-04-02 22:23:48 -0700

    lolwut

--- tools/install.sh
+++ tools/install.sh
@@ -53,18 +53,7 @@ main() {
     echo "Error: git is not installed"
     exit 1
   }
-  env git clone --depth=1 https://github.com/kenzierocks/oh-my-zsh.git $ZSH || {
-=======
-    printf "Error: git clone of oh-my-zsh repo failed\n"
-    exit 1
-  }
-  cwd="$(pwd)"
-  cd $ZSH
-  printf "${BLUE}Attaching fetch-origin...${NORMAL}\n"
-  env git remote add fetch-origin https://github.com/robbyrussell/oh-my-zsh.git
-  cd "$cwd"
 
->>>>>>> Okie dokie, got my own install setup
   # The Windows (MSYS) Git is not compatible with normal use on cygwin
   if [ "$OSTYPE" = cygwin ]; then
     if git --version | grep msysgit > /dev/null; then
@@ -78,6 +67,11 @@ main() {
     exit 1
   }
 
+  cwd="$(pwd)"
+  cd $ZSH
+  printf "${BLUE}Attaching fetch-origin...${NORMAL}\n"
+  env git remote add fetch-origin https://github.com/robbyrussell/oh-my-zsh.git
+  cd "$cwd"
 
   printf "${BLUE}Looking for an existing zsh config...${NORMAL}\n"
   if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
